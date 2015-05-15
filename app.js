d36382b1f5cf053e6de65ca99385d40f6fa5b87d
Wouter <tuvokki@gmail.com>
2015-05-15 22:17:42 +0200

    Starting with app

--- app.js
+++ app.js
@@ -1,41 +1,16 @@
-var nodegit = require('nodegit');
-var promisify = require("promisify-node");
-var fse = promisify(require("fs-extra"));
-var path = "/tmp/Huna_Server";
+var express = require('express');
+var app = express();
 
-fse.remove(path).then(function() {
-  var entry;
-
-  nodegit.Clone(
-    "https://github.com/TuvokVersatileKolinahr/Huna_Server.git",
-    path,
-    {
-      remoteCallbacks: {
-        certificateCheck: function() {
-          // github will fail cert check on some OSX machines
-          // this overrides that check
-          return 1;
-        }
-      }
-    })
-  .done(function() {
-    console.log("Done");
-  });
-  // .then(function(repo) {
-  //   return repo.getCommit("5a1ed0c94f781f6490ebaa216ab6fcdeb8f55e2b");
-  // })
-  // .then(function(commit) {
-  //   return commit.getEntry("README.md");
-  // })
-  // .then(function(entryResult) {
-  //   entry = entryResult;
-  //   return entry.getBlob();
-  // })
-  // .done(function(blob) {
-  //   console.log(entry.filename(), entry.sha(), blob.rawsize() + "b");
-  //   console.log("========================================================\n\n");
-  //   var firstTenLines = blob.toString().split("\n").slice(0, 10).join("\n");
-  //   console.log(firstTenLines);
-  //   console.log("...");
-  // });
+app.get('/', function (req, res) {
+  res.send('updating repo');
 });
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('repos app listening at http://%s:%s', host, port);
+
+});
+
