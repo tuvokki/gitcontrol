8ff8b9d1c1a03eb349ea448e6ccd5ae1a8c6fdab
Wouter <tuvokki@gmail.com>
2015-07-20 15:18:20 +0200

    Dump webhooks in mongo db

--- app.js
+++ app.js
@@ -9,11 +9,53 @@ var router = bogart.router();
 
 router.get('/', function(req) { 
   var article = { };
-  return viewEngine.respond('index.html', article);
+
+    var MongoClient = require('mongodb').MongoClient;
+
+    var url = 'mongodb://';
+    if (process.env.DB_USER) {
+      url = url + process.env.DB_USER+':'+process.env.DB_PASS+'@';
+    }
+    url = url + process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME
+
+    MongoClient.connect(url, function(err, db) {
+        // Find all data
+        collection.find({})
+          // .limit(1)
+          .toArray(function(err, datathings) {
+            db.close();
+            if (err != null) {
+              return viewEngine.respond('index.html', datathings);
+            }
+            else {
+              if (d.length > 0) {
+                d[0].count = d[0].errordata.length;
+              }
+              return viewEngine.respond('index.html', datathings);
+            }
+        });
+    });
+
 });
 
 router.post('/payload', function(req) {
-    console.log(req.body);
+    var MongoClient = require('mongodb').MongoClient;
+
+    var url = 'mongodb://';
+    if (process.env.DB_USER) {
+      url = url + process.env.DB_USER+':'+process.env.DB_PASS+'@';
+    }
+    url = url + process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME
+
+    MongoClient.connect(url, function(err, db) {
+      var collection = db.collection('webhooks');
+      collection.insert(req.body, function(err, result) {
+          if(err)
+              throw err;
+       
+          console.log("entry saved");
+      });
+    });
     return bogart.json({ webhook: 'thanks' });
 });
 
