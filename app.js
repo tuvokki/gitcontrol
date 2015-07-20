c8bf8b2f3d4d25866b3d6580bbb0031de67b0de5
Wouter <tuvokki@gmail.com>
2015-07-20 15:53:22 +0200

    let main function return

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ router.get('/', function(req) {
 
       var collection = db.collection('webhooks');
 
-      collection.find().toArray(function(err, items) {
+      return collection.find().toArray(function(err, items) {
         console.log('found', items.length());
         db.close();
         return viewEngine.respond('index.html', items);
